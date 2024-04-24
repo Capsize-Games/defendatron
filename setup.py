09ab32d38ed03bbc5da787dfebac9e4a708052ee
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2024-04-23 20:14:25 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="defendatron",
-    version="0.1.0",
+    version="0.1.1",
     author="Capsize LLC",
     description="",
     long_description=open("README.md", "r", encoding="utf-8").read(),
